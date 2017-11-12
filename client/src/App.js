ffeade5391ab5093f0263d2691ecee5b49ddc338
uniOpifex <njhunit@gmail.com>
2017-11-12 16:21:07 -0500

    devise setup sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
+import SignUpLogIn from './components/SignUpLogIn'
+import axios from 'axios'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        signedIn: false
+    }
+
+    signUp = async (email, password, password_confirmation) => {
+        try {
+            const payload = {
+                email: email,
+                password: password,
+                password_confirmation: password_confirmation
+            }
+            await axios.post('/auth', payload)
+
+            this.setState({signedIn: true})
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    signIn = async (email, password) => {
+        try {
+            const payload = {
+                email,
+                password
+            }
+            await axios.post('/auth/sign_in', payload)
+
+            this.setState({signedIn: true})
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    render() {
+
+        const SignUpLogInComponent = () => (
+            <SignUpLogIn
+                signUp={this.signUp}
+                signIn={this.signIn}/>
+        )
+
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/signUp" render={SignUpLogInComponent}/>
+                    </Switch>
+
+                    {this.state.signedIn ? null : <Redirect to="/signUp"/>}
+                </div>
+            </Router>
+        )
+    }
 }
 
-export default App;
+export default App
