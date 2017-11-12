738e87c60d4992d7e31244fee7138e5b4545788f
uniOpifex <njhunit@gmail.com>
2017-11-12 16:34:07 -0500

    created post and item model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+          :recoverable, :rememberable, :trackable, :validatable,
+          :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :items, dependent: :destroy
 end
